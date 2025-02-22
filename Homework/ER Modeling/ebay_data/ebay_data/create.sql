50bf007c52ea11d4bcbe92a1868f56b53b25a1a6
Aaron Melcher <amelch130@gmail.com>
2025-02-22 13:46:14 -0600

    Adding setup files

---
+++ Homework/ER Modeling/ebay_data/ebay_data/create.sql
@@ -0,0 +1,40 @@
+drop table if exists Items;
+drop table if exists Bids;
+drop table if exists Users;
+drop table if exists Categories;
+
+create table Items (
+    ItemID          integer primary key,
+    [Name]          text not null,
+    Currently       real not null,
+    Buy_Price       real,
+    First_Bid       real not null,
+    Number_of_Bids  integer not null,
+    SellerID        text not null,
+    [Started]       text not null,
+    Ends            text not null,
+    [Description]   text not null,
+    foreign key (SellerID) references Users(UserID) on delete cascade
+);
+
+create table Bids (
+    ItemID      integer not null,
+    UserID      text not null,
+    [Time]      text not null,
+    Amount      text not null
+    primary key (ItemID, UserID),
+    foreign key (ItemID) references Items(ItemID) on delete cascade,
+    foreign key (UserID) references Users(UserID) on delete cascade
+);
+
+create table Users (
+    UserID      text primary key,
+    Rating      text not null,
+    [Location]  text,
+    Country     text
+);
+
+create table Categories (
+    CategoryID  integer primary key,
+    Category    text not null
+);
