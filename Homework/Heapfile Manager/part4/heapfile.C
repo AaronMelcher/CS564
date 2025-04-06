4968a6813dd03ed9162c61641b5983ce3ea04d91
Aaron Melcher <amelch130@gmail.com>
2025-04-06 15:42:08 -0500

    Cleanup for heapfile.c

--- Homework/Heapfile Manager/part4/heapfile.C
+++ Homework/Heapfile Manager/part4/heapfile.C
@@ -56,7 +56,7 @@ const Status createHeapFile(const string fileName)
         hdrPage->pageCnt = 2;
         hdrPage -> recCnt = 0;
 
-        // Keeping in for now, may not need
+        // Bookeeping
         bufMgr->unPinPage(file, hdrPageNo, true);
         bufMgr->unPinPage(file, newPageNo, true);
         
@@ -559,63 +559,6 @@ const Status InsertFileScan::insertRecord(const Record & rec, RID& outRid)
         headerPage->recCnt++;
         return status;
     }
-
-
-
-    // // Ensure current page is the last page
-    // // If curPage is NULL or not the last page,
-    // // unpin the page and load in the last page
-    // if(curPage == NULL || curPageNo != headerPage->lastPage){
-    //     if(curPage != NULL) {
-    //         unpinstatus = bufMgr->unPinPage(filePtr, curPageNo, curDirtyFlag);
-    //         if(status != OK)
-    //             return status;
-    //     }
-    //     int lastPageNo = headerPage->lastPage;
-    //     status = bufMgr->readPage(filePtr, lastPageNo, curPage);
-    //     if(status != OK);
-    //         return status;
-    //     curPageNo = lastPageNo;
-    //     curDirtyFlag = false;
-    // }
-
-    // // Attempt to insert the record into the last page
-    // status = curPage->insertRecord(rec, outRid);
-
-    // // Alloc new page if insufficient space
-    // if(status == NOSPACE){
-    //     status = bufMgr->allocPage(filePtr, newPageNo, newPage);
-    //     if(status != OK)
-    //         return status;
-        
-    //     // Init new page
-    //     newPage->init(newPageNo);
-    //     headerPage->lastPage = newPageNo;
-    //     headerPage->pageCnt++;
-    //     hdrDirtyFlag = true;
-
-    //     // Unpin current page
-    //     unpinstatus = bufMgr->unPinPage(filePtr, curPageNo, curDirtyFlag);
-    //     if(status != OK)
-    //         return status;
-        
-    //     // Set new page as current
-    //     curPage = newPage;
-    //     curPageNo = newPageNo;
-    //     curDirtyFlag = false;
-
-    //     // Attempt again to insert the record
-    //     status = curPage->insertRecord(rec, outRid);
-    //     if(status != OK)
-    //         return status;
-    // } else if (status != OK){
-    //     return status; // return any non-NOSPACE errors
-    // }
-
-    // headerPage->recCnt++;
-    // hdrDirtyFlag = true;
-
-    // return OK;
 }
 
 
