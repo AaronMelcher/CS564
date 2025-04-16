f3c51e102d88764a3a789254cd8515e4f986c3b7
Aaron Melcher <amelch130@gmail.com>
2025-04-16 15:48:47 -0500

    implementing insert.C

--- Homework/Query Operators/part6/insert.C
+++ Homework/Query Operators/part6/insert.C
@@ -14,8 +14,72 @@ const Status QU_Insert(const string & relation,
 	const int attrCnt, 
 	const attrInfo attrList[])
 {
-// part 6
-return OK;
+	cout << "Doing QU_Insert on relation " << relation << endl;
+	Status status;
 
+	// Retrieve full schema for this relation
+	int fullAttrCnt = 0;
+	AttrDesc *fullAttrs = nullptr;
+	status = attrCat->getRelInfo(relation, fullAttrCnt, fullAttrs);
+	if(status!=OK){
+		cout << "Error: could not retrieve schema for " << relation << endl;
+		return status;
+	}
+
+	// compute record length
+	int recLen = 0;
+	for(int i = 0; i < fullAttrCnt; i++){
+		recLen += fullAttrs[i].attrLen;
+	}
+
+	// allocate buffer for new record
+	char *tuple = new char[recLen];
+
+	// find all values for each attribute in the schema
+	// and copy into the tuple buffer
+	for(int i = 0; i < fullAttrCnt; i++){
+		const char *attrName = fullAttrs[i].attrName;
+		bool found = false;
+
+		for(int j = 0; j < attrCnt; j++){
+			if(strcmp(attrList[j].attrName, attrName) == 0){
+				// Copy binary val
+				memcpy(tuple + fullAttrs[i].attrOffset, 
+						attrList[j].attrValue, fullAttrs[i].attrLen);
+				found = true;
+				break;
+			}
+		}
+
+		if(!found){
+			cout << "Error: missing value for attribute" << endl;
+			delete [] tuple;
+			delete [] fullAttrs;
+			return ATTRNOTFOUND; // is this the right code?
+		}
+
+		// open InsertFileScan
+		InsertFileScan insert(relation, status);
+		if(status != OK) {
+			cout << "Error: unable to open InsertFileScan" << endl;
+			delete [] tuple;
+			delete [] fullAttrs;
+			return status;
+		}
+
+		Record rec;
+		rec.data = (void*) tuple;
+		rec.length = recLen;
+		RID outRid;
+
+		status = insert.insertRecord(rec, outRid);
+		if(status != OK){
+			cout << "Error: failed to insert record" << endl;
+		}
+
+		delete [] tuple;
+		delete [] fullAttrs;
+		return status;
+	}
 }
 
