08cbe23d61df23cd0164fbb56e17e02bd7846577
ckrmpotich <krmpotich@vm-instunix-02.cs.wisc.edu>
2025-04-17 17:27:23 -0500

    Implementing delete.C

--- Homework/Query Operators/part6/delete.C
+++ Homework/Query Operators/part6/delete.C
@@ -16,10 +16,50 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
 
+	// part 6
 
+	// passed relation, attrName, operator, type, pointer to attribute value
+
+	cout << "Doing QU_Delete on relation " << relation << "attrName: " << attrName << " OP: " << op << "type: " <<type << " attrValue: " << attrValue << endl;
+	Status status;
+
+	// This function will delete all tuples in relation satisfying the predicate specified by attrName, op, and the constant attrValue. 
+	// type denotes the type of the attribute. You can locate all the qualifying tuples using a filtered HeapFileScan.
+
+	HeapFileScan scan(relation, status);
+
+	if (attrName == "") {
+		status = scan.startScan(0, 0, type, nullptr, op);
+	} else {
+		AttrDesc attr;
+		status = attrCat->getInfo(relation, attrName, attr);
+		if (status != OK) {
+			return status;
+		}
+
+		cout << "attr.attrType: " << attr.attrType << "\n";
+
+		status = scan.startScan(attr.attrOffset, attr.attrLen, type, attrValue, op);
+		if (status != OK) {
+			return status;
+		}
+	}
+
+	// Loop over matches and delete them
+    RID rid;
+    while ((status = scan.scanNext(rid)) == OK) {
+        status = scan.deleteRecord();
+        if (status != OK) {
+            scan.endScan();
+            return status;
+        }
+    }
+
+    // End scan
+    scan.endScan();
+
+	return OK;
 
 }
 
