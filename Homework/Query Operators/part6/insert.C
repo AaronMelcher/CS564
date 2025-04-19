6872b31991cc39fbf94e3033bbb0f05d7622ca2c
ckrmpotich <krmpotich@snares-08.cs.wisc.edu>
2025-04-19 15:29:57 -0500

    Updated insert.C

--- Homework/Query Operators/part6/insert.C
+++ Homework/Query Operators/part6/insert.C
@@ -34,52 +34,65 @@ const Status QU_Insert(const string & relation,
 
 	// allocate buffer for new record
 	char *tuple = new char[recLen];
+	bool found = false;
 
 	// find all values for each attribute in the schema
 	// and copy into the tuple buffer
 	for(int i = 0; i < fullAttrCnt; i++){
 		const char *attrName = fullAttrs[i].attrName;
-		bool found = false;
 
-		for(int j = 0; j < attrCnt; j++){
-			if(strcmp(attrList[j].attrName, attrName) == 0){
-				// Copy binary val
-				memcpy(tuple + fullAttrs[i].attrOffset, 
-						attrList[j].attrValue, fullAttrs[i].attrLen);
+		for(int j = 0; j < attrCnt; j++) {
+
+			Datatype type = (Datatype)attrList[j].attrType;
+			const char *attrValue = (char*)attrList[j].attrValue;
+
+			if (type == INTEGER) {
+				// make integer
+				int intVal = std::atoi(attrValue);
+				memcpy(tuple + fullAttrs[i].attrOffset, (char*)&intVal, fullAttrs[i].attrLen);
 				found = true;
-				break;
+			} else if (type == FLOAT) {
+				// make float
+				float fltVal = std::atof(attrValue);
+				memcpy(tuple + fullAttrs[i].attrOffset, (char*)&fltVal, fullAttrs[i].attrLen);
+                found = true;
+			} else {
+				memcpy(tuple + fullAttrs[i].attrOffset, attrValue, fullAttrs[i].attrLen);
+				found = true;
 			}
+			break;
 		}
+	}
 
-		if(!found){
-			cout << "Error: missing value for attribute" << endl;
-			delete [] tuple;
-			delete [] fullAttrs;
-			return ATTRNOTFOUND; // is this the right code?
-		}
+	if(!found){
+		cout << "Error: missing value for attribute" << endl;
+		delete [] tuple;
+		delete [] fullAttrs;
+		return ATTRNOTFOUND; // is this the right code?
+	}
 
-		// open InsertFileScan
-		InsertFileScan insert(relation, status);
-		if(status != OK) {
-			cout << "Error: unable to open InsertFileScan" << endl;
-			delete [] tuple;
-			delete [] fullAttrs;
-			return status;
-		}
-
-		Record rec;
-		rec.data = (void*) tuple;
-		rec.length = recLen;
-		RID outRid;
-
-		status = insert.insertRecord(rec, outRid);
-		if(status != OK){
-			cout << "Error: failed to insert record" << endl;
-		}
-
+		
+	// open InsertFileScan
+	InsertFileScan insert(relation, status);
+	if(status != OK) {
+		cout << "Error: unable to open InsertFileScan" << endl;
 		delete [] tuple;
 		delete [] fullAttrs;
 		return status;
 	}
+
+	Record rec;
+	rec.data = (void*) tuple;
+	rec.length = recLen;
+	RID outRid;
+
+	status = insert.insertRecord(rec, outRid);
+	if(status != OK){
+		cout << "Error: failed to insert record" << endl;
+	}
+
+	delete [] tuple;
+	delete [] fullAttrs;
+	return status;
 }
 
